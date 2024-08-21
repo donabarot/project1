1d375c248e858648d02a98108f9d41838d8a0878
donabarot <barotdona4@gmail.com>
2024-08-21 17:30:17 +0530

    Updated project files, deleted unused images, and added new video and JS file

---
+++ project1.js
@@ -0,0 +1,37 @@
+
+var crsr = document.querySelector("#cursor") 
+var blur = document.querySelector("#cursor-blur") //querySelector is used to access a div from html to js
+document.addEventListener("mousemove",function(dets){ //dets is a parameter we pass in a function to check mousemovements
+    crsr.style.left = dets.x+"px",
+    crsr.style.top = dets.y+"px" 
+    blur.style.left = dets.x-200+"px",
+    blur.style.top = dets.y-200+"px"
+})
+
+gsap.to("#nav",{
+    backgroundColor : "#c6f020",
+    durarion: 1,
+    delay:1,
+    height:"70px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:2,
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor:"#c6f020",
+    // duration:1,
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers: true,
+        start:"top -75%",
+        end: "top -100%",
+        scrub:5,
+    }
+})
